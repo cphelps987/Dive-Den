d351b4f38907841195dd568a2a0230b9115dc098
cphelps987 <cphelps987@gmail.com>
2017-06-07 10:26:37 -0400

    added google maps finally working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,26 +2,4 @@ $(document).ready(function () {
 
     console.log("ready!");
 
-    var authKey = "AIzaSyClbAC2i9F_DocCTnfZokjM12bMdsT7bSs";
-    var queryURL = "https://maps.googleapis.com/maps/api/js?key=" + authKey + "&callback=initMap";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).done(function () {
-
-        function initMap() {
-            var uluru = {lat: -25.363, lng: 131.044};
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 4,
-                center: uluru
-            });
-            var marker = new google.maps.Marker({
-                position: uluru,
-                map: map
-            });
-        }
-    });
-});
-
 
