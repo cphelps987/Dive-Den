8b0f10369c44cb6ae3c34cb9a377d92dbce64fdf
gabietaylor <gabie.taylor@yahoo.com>
2017-06-08 17:12:42 -0400

    api is logging now the fun part choosing params

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
 
 console.log('ready1!');
+
+var APIKey = "166a433c57516f51dfab1f7edaed8413";
+
+var queryURL = "http://api.openweathermap.org/data/2.5/forecast?" +
+  "q=Orlando,ISO 3166&mode=xml&units=imperial&appid=" + APIKey;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+	console.log(queryURL);
+    console.log(response);
