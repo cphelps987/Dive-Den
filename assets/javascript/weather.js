61ae35b351af175ff8bafe3751a6605e1131c2e0
gabietaylor <gabie.taylor@yahoo.com>
2017-06-08 18:08:52 -0400

    everything is logging correctly will now add other params

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,15 +1,15 @@
-$(document).ready(function () {
-
-console.log('ready1!');
-
 var APIKey = "166a433c57516f51dfab1f7edaed8413";
 
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?" +
-  "q=Orlando,ISO 3166&mode=xml&units=imperial&appid=" + APIKey;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?" +
+  "q=Orlando,ORL&units=imperial&appid=" + APIKey;
 
 $.ajax({
     url: queryURL,
     method: "GET"
   }).done(function(response) {
-	console.log(queryURL);
+    console.log(queryURL);
     console.log(response);
+
+    $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+    console.log("Wind Speed: " + response.wind.speed);
+});
