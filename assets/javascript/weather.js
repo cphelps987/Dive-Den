604a5e286d94e6c8cdaaecde55c4b5cd223db1f8
gabietaylor <gabie.taylor@yahoo.com>
2017-06-09 12:59:45 -0400

    got the footer to change now trying the header

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -23,3 +23,22 @@ $.ajax({
     console.log("Min. Temperature: " + response.main.temp_min);
     console.log("Max Temperature: " + response.main.temp_max);
 });
+
+  $(document).ready(function() {
+  $.simpleWeather({
+    location: 'Orlando, FL',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+  
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+});
