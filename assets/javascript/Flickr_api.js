c23d6680e6cd5341e269218300bba63a0d2bb46c
cphelps987 <cphelps987@gmail.com>
2017-06-09 00:39:09 -0400

    added ebay test that worked - now we need it to work on out html also was messing with flickr but commented it out - added JS file to index - added var to divesite

--- assets/javascript/Flickr_api.js
+++ assets/javascript/Flickr_api.js
@@ -2,11 +2,13 @@
 
 console.log("flickr ready!")
 
+//var flickrLoopUp= diveResults.sites[1].name;
+/*$.getJSON("https://cors-bcs.herokuapp.com/https://api.flickr.com/services/feeds/photos_public.gne?tags=florida,scuba,"+ flickrLoopUp +"&format=json&nojsoncallback=1", function(data){*/
 
 $.getJSON("https://cors-bcs.herokuapp.com/https://api.flickr.com/services/feeds/photos_public.gne?tags=florida,scuba&format=json&nojsoncallback=1", function(data){
     $.each(data.items, function(i,item){
         $("<img>").attr("src", item.media.m).appendTo("#flickrImg")
-            .wrap("<a href='" + item.link + "'></a>");
+          //  .wrap("<a href='" + item.link + "'></a>");
     });
 
 });
